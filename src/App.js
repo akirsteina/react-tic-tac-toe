1c0c16ed83b32fb2ad34988df3959431328dd93e
akirsteina <anna.kirsteina@gmail.com>
2021-11-16 10:09:09 +0200

    add initial training code and transform from class to functional components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import React from 'react';
+import Game from './Components/Game';
+
+import './index.css';
+
 function App() {
-	return <div>Test</div>;
+	return (
+		<React.Fragment>
+			<Game />
+		</React.Fragment>
+	);
 }
 
 export default App;
---
+++ src/Components/Square.js
@@ -0,0 +1,5 @@
+const Square = () => {
+	return <button className='square'>{/* TODO */}</button>;
+};
+
+export default Square;
